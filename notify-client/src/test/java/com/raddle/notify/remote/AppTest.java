c7e662f48aa707b8e591c1bad8e4ceffcb861d84
raddle60 <raddle60@gmail.com>
2011-04-25 05:17:49 +0000

    加入xstream

--- notify-client/src/test/java/com/raddle/notify/remote/AppTest.java
+++ notify-client/src/test/java/com/raddle/notify/remote/AppTest.java
@@ -1,8 +1,14 @@
 package com.raddle.notify.remote;
 
+import java.util.ArrayList;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import com.raddle.notify.remote.bean.PositionColor;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 /**
  * Unit test for simple App.
@@ -33,6 +39,11 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+    	XStream xstream = new XStream(new DomDriver());
+    	xstream.alias("postion-color", ArrayList.class);
+    	xstream.alias("item", PositionColor.class);
+    	@SuppressWarnings("unchecked")
+		ArrayList<PositionColor> list =  (ArrayList<PositionColor>) xstream.fromXML(AppTest.class.getResourceAsStream("/postion-color.xml"));
+    	System.out.println(list.get(0).getColor());
     }
 }
